b695311f028a2f75f7b6342ba1e54f96d72f243d
petoGriac <peter.griac@garwan.sk>
2023-10-17 11:51:17 +0200

    Creating prisma schema

---
+++ prisma/migrations/20231017094733_init/migration.sql
@@ -0,0 +1,94 @@
+-- CreateEnum
+CREATE TYPE "JobRole" AS ENUM ('FRONTEND_DEVELOPER', 'BACKEND_DEVELOPER');
+
+-- CreateTable
+CREATE TABLE "person" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "status" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "note" TEXT,
+    "job_role" "JobRole" NOT NULL,
+    "not_billable" BOOLEAN NOT NULL,
+    "level" INTEGER NOT NULL,
+
+    CONSTRAINT "person_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "availability" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "percentage" INTEGER NOT NULL,
+    "date_from" TIMESTAMP(3) NOT NULL,
+    "personId" INTEGER NOT NULL,
+    "pm_personId" INTEGER NOT NULL,
+
+    CONSTRAINT "availability_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "project" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "status" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "note" TEXT,
+    "color" TEXT NOT NULL,
+
+    CONSTRAINT "project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "allocation" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "percentage" INTEGER NOT NULL,
+    "date_from" TIMESTAMP(3) NOT NULL,
+    "date_to" TIMESTAMP(3),
+    "note" TEXT,
+    "personId" INTEGER NOT NULL,
+    "projectId" INTEGER NOT NULL,
+
+    CONSTRAINT "allocation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tag" (
+    "id" SERIAL NOT NULL,
+    "value" TEXT NOT NULL,
+
+    CONSTRAINT "tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TagsOnPersons" (
+    "personId" INTEGER NOT NULL,
+    "tagId" INTEGER NOT NULL,
+
+    CONSTRAINT "TagsOnPersons_pkey" PRIMARY KEY ("personId","tagId")
+);
+
+-- AddForeignKey
+ALTER TABLE "availability" ADD CONSTRAINT "availability_personId_fkey" FOREIGN KEY ("personId") REFERENCES "person"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "availability" ADD CONSTRAINT "availability_pm_personId_fkey" FOREIGN KEY ("pm_personId") REFERENCES "person"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "allocation" ADD CONSTRAINT "allocation_personId_fkey" FOREIGN KEY ("personId") REFERENCES "person"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "allocation" ADD CONSTRAINT "allocation_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "project"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TagsOnPersons" ADD CONSTRAINT "TagsOnPersons_personId_fkey" FOREIGN KEY ("personId") REFERENCES "person"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TagsOnPersons" ADD CONSTRAINT "TagsOnPersons_tagId_fkey" FOREIGN KEY ("tagId") REFERENCES "tag"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
